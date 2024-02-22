e6ea061358763dffa7258080ce6dee4be2a88b5c
Akayy <techakayy@gmail.com>
2024-02-22 14:57:45 +1100

    Hooked up themes, updated primary, reload works nicely

--- playground/pages/index.vue
+++ playground/pages/index.vue
@@ -5,25 +5,22 @@
     </div>
     <div :class="mainDivClass">
       <div>
-        <span class="pr-1 font-medium">This is a HMR test, try changing the color:</span>
+        <span class="pr-1 font-medium"
+          >This is a HMR test, try changing the color:</span
+        >
         <span class="text-blue-500">meow!</span>
       </div>
-      <p class="text-brand">
-        This color comes from the `./tailwind.config`
-      </p>
-      <p class="text-accent">
-        This color comes from the `./theme` layer
-      </p>
+      <p class="text-primary">This color comes from the `./tailwind.config`</p>
+      <p class="text-accent">This color comes from the `./theme` layer</p>
       <div>
-        <NuxtLink
-          to="/content"
-          class="underline hover:text-indigo-500"
-        >
+        <NuxtLink to="/content" class="underline hover:text-indigo-500">
           Visit the /content page to check out the @nuxt/content integration!
         </NuxtLink>
       </div>
       <div>
-        <span class="text-sm font-semibold text-gray-700">Resolved tailwind config:</span>
+        <span class="text-sm font-semibold text-gray-700"
+          >Resolved tailwind config:</span
+        >
         <textarea
           class="p-4 block text-sm border border-gray-100 rounded shadow w-full h-[32rem] font-mono outline-none"
           readonly
@@ -35,9 +32,9 @@
 </template>
 
 <script setup lang="ts">
-import { tw } from '#imports'
-import tailwindConfig from '#tailwind-config'
+import { tw } from "#imports";
+import tailwindConfig from "#tailwind-config";
 
 // const tw = <T>(s: T) => s
-const mainDivClass = tw`max-w-screen-lg p-4 mx-auto space-y-4`
+const mainDivClass = tw`max-w-screen-lg p-4 mx-auto space-y-4`;
 </script>
--- playground/tailwind.config.ts
+++ playground/tailwind.config.ts
@@ -1,18 +1,22 @@
-import typography from '@tailwindcss/typography'
-import colors from 'tailwindcss/colors'
+import typography from "@tailwindcss/typography";
+import colors from "tailwindcss/colors";
+import {
+  pg_colors,
+  pg_fonts,
+  pg_backgrounds,
+} from "./themes/pg-tailwindcss/tokens.cjs";
 
 export default {
   theme: {
     extend: {
-      colors: {
-        brand: colors.teal['500']
-      },
+      colors: { ...pg_colors, pp: pg_colors.primary },
+      // colors: {
+      //   brand: colors.teal['500']
+      // },
       fontFamily: {
-        sans: 'Inter, ui-sans-serif, system-ui, -apple-system, Arial, Roboto, sans-serif'
-      }
-    }
+        sans: "Inter, ui-sans-serif, system-ui, -apple-system, Arial, Roboto, sans-serif",
+      },
+    },
   },
-  plugins: [
-    typography()
-  ]
-}
+  plugins: [typography()],
+};
